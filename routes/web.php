c57490b2716dc75688d672a14dd6f4b44d5af26b
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2025-09-18 17:14:07 +0600

    role auth

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController; 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', ProductController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
